fb148b880210b07fc1538dd7e6e249181fa466e7
prowebdev110 <tieosong119@gmail.com>
2022-10-20 02:07:29 +0900

    Update account.js

--- template/js/account.js
+++ template/js/account.js
@@ -1,7 +1,11 @@
 $( window ).resize(function() {
+	//var size = $(window).height()-150;
+	//$('.main-account').css({'min-height':size+'px'});
 });
 
 $(window).on('load', function () {
+	//var size = $(window).height()-150;
+	//$('.main-account').css({'min-height':size+'px'});
 });
 
 
